864d695d00c60a4e56fd1420e3e7bd46ea965bd7
xiaoqinglin <xiaoqinglin2018@gmail.com>
2020-11-10 08:47:29 +0800

    feat(1.0.0): 3_1_prototype design

---
+++ design_patterns/3_1_prototype_pattern/prototype_pattern.py
@@ -0,0 +1,44 @@
+import copy
+
+
+class WebSite:
+    def __init__(self, name, domain, description, author, **kwargs):
+        """
+        kwargs: category creation_date technologies keywords
+        """
+        self.name = name
+        self.domain = domain
+        self.description = description
+        self.author = author
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    def __str__(self):
+        summary = [f'WebSite "{self.name}"\n']
+        infos = vars(self).items()
+        ordered_infos = sorted(infos)
+        for attr, val in ordered_infos:
+            if attr == 'name':
+                continue
+            summary.append(f'{attr}: {val} \n')
+        return ''.join(summary)
+
+
+class Prototype:
+    def __init__(self):
+        self.objects = dict()
+
+    def register(self, identifier, obj):
+        self.objects[identifier] = obj
+
+    def unregister(self, identifier):
+        del self.objects[identifier]
+
+    def clone(self, identifier, **attrs):
+        found = self.objects.get(identifier)
+        if not found:
+            raise ValueError(f'Incorrect object identifier: {identifier}')
+        obj = copy.deepcopy(found)
+        for key in attrs:
+            setattr(obj, key, attrs[key])
+        return obj
